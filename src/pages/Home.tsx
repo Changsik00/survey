3afa6a9b11206d1beb7345619c366e1008dfb97a
changsik <lowmans00@gmail.com>
2025-03-02 21:05:24 +0900

    clsx, tailwind-merge 으로 cn 만들고 Button 컴포넌트에 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '@/components/common/Button';
+
 export default function Home() {
-  return <div className="text-3xl font-bold underline">Home Page - Template List(Temporary Text)</div>;
+  const navigate = useNavigate();
+
+  const goToCreateTemplate = () => {
+    navigate('/template/create');
+  };
+  return (
+    <>
+      <div className="text-3xl font-bold underline">Home Page - Template List(Temporary Text)</div>
+      <Button onClick={goToCreateTemplate} primary>
+        Go Template Create
+      </Button>
+    </>
+  );
 }
