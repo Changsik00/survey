fac112e6259d43fc7cce2e09fc7d6b030071b2f1
changsik <lowmans00@gmail.com>
2025-03-03 18:32:35 +0900

    refactor: path alias 사용

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import Layout from './components/common/Layout';
-import ResponsiveTest from './components/ResponsiveTest';
-import Home from './pages/Home';
-import Template from './pages/Template';
-import TemplateCreate from './pages/TemplateCreate';
+import Layout from '@/components/common/Layout';
+import ResponsiveTest from '@/components/ResponsiveTest';
+import Home from '@/pages/Home';
+import Template from '@/pages/Template';
+import TemplateCreate from '@/pages/TemplateCreate';
 
-import './app.css';
+import '@/app.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
